eee07aeaadaf6f34efec01b9183763ec3c6eeb37
Christopher Ram <85909489+ChrisRam30@users.noreply.github.com>
2023-05-22 00:29:22 -0700

    Add files via upload

---
+++ Migrations/20230522013110_NewMovies.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movies.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "MovieName",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "MovieName",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "MovieName",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "MovieName",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "MovieName",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "MovieName",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Data;
+
+namespace Movies.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MoviesContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MoviesContext>>()))
+        {
+            if (context == null || context.MovieName == null)
+            {
+                throw new ArgumentNullException("Null MoviesContext");
+            }
+
+            // Look for any movies.
+            if (context.MovieName.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.MovieName.AddRange(
+                new MovieName
+                {
+                    Title = "Pacific Rim",
+                    ReleaseDate = DateTime.Parse("2011-2-12"),
+                    Genre = "Action",
+                    Price = 8.99M,
+                    Rating = "PG-13"
+                },
+
+                new MovieName
+                {
+                    Title = "A Knights Tale ",
+                    ReleaseDate = DateTime.Parse("2004-3-13"),
+                    Genre = "Drama",
+                    Price = 9.99M,
+                    Rating = "PG-13"
+                },
+
+                new MovieName
+                {
+                    Title = "Shawshank Redemption",
+                    ReleaseDate = DateTime.Parse("1999-2-23"),
+                    Genre = "Drama",
+                    Price = 9.99M,
+                    Rating = "R"
+                },
+
+                new MovieName
+                {
+                    Title = "Batman Begins",
+                    ReleaseDate = DateTime.Parse("2009-4-15"),
+                    Genre = "Action",
+                    Price = 10.99M,
+                    Rating = "PG-13"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
